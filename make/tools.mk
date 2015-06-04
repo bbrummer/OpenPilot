e6903191b27d67fd7772d7a858d49f7a2696bde3
James Duley <jagduley@gmail.com>
2015-06-04 13:34:59 +0100

    OP-1886 smarter sdk_install target: rework file download targets

--- make/tools.mk
+++ make/tools.mk
@@ -294,22 +294,35 @@ endif
 # Cross platform download template
 #  $(1) = Package URL
 #  $(2) = Package file
-#  $(3) = URL for .md5 file to be tested against Package
 #
 ##############################
 
 define DOWNLOAD_TEMPLATE
-@$(ECHO) $(MSG_VERIFYING) $$(call toprel, $(DL_DIR)/$(2))
-	$(V1) ( \
-		cd "$(DL_DIR)" && \
-		$(CURL) $(CURL_OPTIONS) --silent -o "$(DL_DIR)/$(2).md5" "$(3)" && \
-		if [ $(call MD5_CHECK_TEMPLATE,$(DL_DIR)/$(2),!=) ]; then \
-			$(ECHO) $(MSG_DOWNLOADING) $(1) && \
-			$(CURL) $(CURL_OPTIONS) -o "$(DL_DIR)/$(2)" "$(1)" && \
-			$(ECHO) $(MSG_CHECKSUMMING) $$(call toprel, $(DL_DIR)/$(2)) && \
-			[ $(call MD5_CHECK_TEMPLATE,$(DL_DIR)/$(2),=) ]; \
-		fi; \
-	)
+$(DL_DIR)/$(2): | $(DL_DIR)
+	@$(ECHO) $(MSG_DOWNLOADING) $$(call toprel, $(DL_DIR)/$(2))
+	$(V1) $(CURL) $$(CURL_OPTIONS) -o "$(DL_DIR)/$(2)" "$(1)"
+endef
+
+##############################
+#
+# Cross platform download and check template
+#  $(1) = Package URL
+#  $(2) = Package file
+#  $(3) = URL for .md5 file to be tested against Package
+#
+##############################
+
+define DOWNLOAD_AND_CHECK_TEMPLATE
+$(DL_DIR)/$(2).md5: CURL_OPTIONS += --silent
+
+$(call DOWNLOAD_TEMPLATE,$(3),$(2).md5)
+
+$(DL_DIR)/$(2): $(DL_DIR)/$(2).md5
+
+$(call DOWNLOAD_TEMPLATE,$(1),$(2))
+	@$(ECHO) $(MSG_VERIFYING) $$(call toprel, $(DL_DIR)/$(2))
+	$(V1) $(call MD5_CHECK_TEMPLATE,$(DL_DIR)/$(2),=) || \
+		( mv $(DL_DIR)/$(2) $(DL_DIR)/$(2).failed && false)
 endef
 
 ##############################
@@ -329,10 +342,9 @@ define TOOL_INSTALL_TEMPLATE
 
 .PHONY: $(addprefix $(1)_, install clean distclean)
 
-$(1)_install: $(1)_clean | $(DL_DIR) $(TOOLS_DIR)
-	
-	$(if $(4), $(call DOWNLOAD_TEMPLATE,$(3),$(5),$(4)),$(call DOWNLOAD_TEMPLATE,$(3),$(5),"$(3).md5"))
-	
+$(if $(4), $(call DOWNLOAD_AND_CHECK_TEMPLATE,$(3),$(5),$(4)),$(call DOWNLOAD_TEMPLATE,$(3),$(5)))
+
+$(1)_install: $(1)_clean $(DL_DIR)/$(5) | $(TOOLS_DIR)
 	@$(ECHO) $(MSG_EXTRACTING) $$(call toprel, $(2))
 	$(V1) $(MKDIR) -p $$(call toprel, $(dir $(2)))
 
@@ -374,12 +386,13 @@ define WIN_QT_INSTALL_TEMPLATE
 
 .PHONY: $(addprefix qt_sdk_, install clean distclean)
 
-qt_sdk_install: qt_sdk_clean | $(DL_DIR) $(TOOLS_DIR)
+$(call DOWNLOAD_TEMPLATE,$(3),$(5),$(4))
+
+qt_sdk_install: qt_sdk_clean $(DL_DIR)/$(5) | $(TOOLS_DIR)
 	$(V1) if ! $(SEVENZIP) >/dev/null 2>&1; then \
 		$(ECHO) $(MSG_NOTICE) "Missing 7zip. Run ./make/scripts/win_sdk_install.sh [<OpenPilot tools dir>] to get it." && \
 		exit 1; \
 	fi
-	$(call DOWNLOAD_TEMPLATE,$(3),$(5),"$(4)")
 # Explode .run file into install packages
 	@$(ECHO) $(MSG_EXTRACTING) $$(call toprel, $(1))
 	$(V1) $(MKDIR) -p $$(call toprel, $(dir $(1)))
@@ -438,12 +451,13 @@ define LINUX_QT_INSTALL_TEMPLATE
 
 .PHONY: $(addprefix qt_sdk_, install clean distclean)
 
-qt_sdk_install: qt_sdk_clean | $(DL_DIR) $(TOOLS_DIR)
+$(call DOWNLOAD_TEMPLATE,$(3),$(5),$(4))
+
+qt_sdk_install: qt_sdk_clean $(DL_DIR)/$(5) | $(TOOLS_DIR)
 	$(V1) if ! $(SEVENZIP) >/dev/null 2>&1; then \
 		$(ECHO) $(MSG_NOTICE) "Please install the p7zip for your distribution. i.e.: sudo apt-get install p7zip-full" && \
 		exit 1; \
 	fi
-	$(call DOWNLOAD_TEMPLATE,$(3),$(5),"$(4)")
 # Explode .run file into install packages
 	@$(ECHO) $(MSG_EXTRACTING) $$(call toprel, $(1))
 	$(V1) $(MKDIR) -p $$(call toprel, $(dir $(1)))
@@ -501,12 +515,13 @@ define MAC_QT_INSTALL_TEMPLATE
 
 .PHONY: $(addprefix qt_sdk_, install clean distclean)
 
-qt_sdk_install: qt_sdk_clean | $(DL_DIR) $(TOOLS_DIR)
+$(call DOWNLOAD_TEMPLATE,$(3),$(5),$(4))
+
+qt_sdk_install: qt_sdk_clean $(DL_DIR)/$(5) | $(TOOLS_DIR)
 	$(V1) if ! $(SEVENZIP) >/dev/null 2>&1; then \
 		$(ECHO) $(MSG_NOTICE) "Please install the p7zip for your distribution. i.e.: brew install p7zip." && \
 		exit 1; \
 	fi
-	$(call DOWNLOAD_TEMPLATE,$(3),$(5),"$(4)")
 # Mount .dmg file
 	$(V1) hdiutil attach -nobrowse $(DL_DIR)/$(5)
 # Explode .dmg file into install packages
